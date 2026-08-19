24aafe25206509a1671d444d080bc5d764819eb5
Marcin Wierzbiński <marcin119a@gmail.com>
2026-08-19 13:27:57 +0200

    Implement code changes to enhance functionality and improve performance

--- utils/utils.py
+++ utils/utils.py
@@ -2,6 +2,8 @@ import numpy as np
 import pandas as pd
 
 FILES = [
+    'COSMIC_v3.6_SBS_GRCh38.txt',
+    'COSMIC_v3.5_SBS_GRCh38.txt',
     'COSMIC_v3.4_SBS_GRCh38.txt',
     'COSMIC_v3.3.1_SBS_GRCh38.txt',
     'COSMIC_v3.2_SBS_GRCh38.txt',
@@ -10,6 +12,8 @@ FILES = [
     'COSMIC_v2_SBS_GRCh38.txt',
     'COSMIC_v1_SBS_GRCh38.txt',
 
+    'COSMIC_v3.6_SBS_GRCh37.txt',
+    'COSMIC_v3.5_SBS_GRCh37.txt',
     'COSMIC_v3.4_SBS_GRCh37.txt',
     'COSMIC_v3.3.1_SBS_GRCh37.txt',
     'COSMIC_v3.2_SBS_GRCh37.txt',
